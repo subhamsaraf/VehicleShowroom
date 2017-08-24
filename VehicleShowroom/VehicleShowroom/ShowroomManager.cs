fe432df81602d618ed02cae4d5a099f1493dd44f
ssaraf <ssaraf@tavisca.com>
2017-08-24 15:58:11 +0530

    Added the Log Of Each Transiction

--- VehicleShowroom/VehicleShowroom/ShowroomManager.cs
+++ VehicleShowroom/VehicleShowroom/ShowroomManager.cs
@@ -9,11 +9,22 @@ namespace VehicleShowroom
     public class ShowroomManager
     { 
         public double TotalRevenue { get; private set; }
-        public double GenerateBill(Vehicle vehicle,TypeOfService typeOfService,PriceList priceList)
+        public List<ServiceLog> Services = new List<ServiceLog>();
+        public double GenerateBill(Vehicle vehicle,TypeOfService typeOfService,PriceList priceList,double hoursOfServices)
         {
             double price = priceList.GetPrice(vehicle.ModelNumber, typeOfService);
+            price = price * hoursOfServices;
             TotalRevenue += price;
+            ServiceLog serviceLog = new ServiceLog()
+            {
+                HoursOfService = hoursOfServices,
+                PriceOfTheService = price,
+                typeOfService = typeOfService,
+                typeOfVehicle = vehicle.Type
+            };
+            Services.Add(serviceLog);
             return price;
+
         }
 
     }
--- VehicleShowroom/VehicleShowroomTest/ShowroomManagerFixture.cs
+++ VehicleShowroom/VehicleShowroomTest/ShowroomManagerFixture.cs
@@ -8,7 +8,7 @@ namespace VehicleShowroomTest
     public class ShowroomManagerFixture
     {
         [TestMethod]
-        public void check_if_we_get_the_correct_price_from_the_manager()
+        public void Check_if_we_get_the_correct_price_from_the_manager()
         {
             Vehicle vehicle = new Vehicle()
             {
@@ -19,8 +19,24 @@ namespace VehicleShowroomTest
             PriceList pricelist = new PriceList();
             pricelist.Add("a1b2c3", 500, TypeOfService.Rent);
             pricelist.Add("1234556", 120000, TypeOfService.Sell);
-            double price = manager.GenerateBill(vehicle, TypeOfService.Rent,pricelist);
-            Assert.AreEqual(500,price);
+            double price = manager.GenerateBill(vehicle, TypeOfService.Rent,pricelist,5);
+            Assert.AreEqual(2500,price);
+        }
+        [TestMethod]
+        public void Manager_keeps_a_record_of_total_revenue()
+        {
+            Vehicle vehicle = new Vehicle()
+            {
+                ModelNumber = "a1b2c3",
+                Type = VehicleType.Car,
+            };
+            ShowroomManager manager = new ShowroomManager();
+            PriceList pricelist = new PriceList();
+            pricelist.Add("a1b2c3", 500, TypeOfService.Rent);
+            pricelist.Add("a1b2c3", 120000, TypeOfService.Sell);
+            manager.GenerateBill(vehicle, TypeOfService.Rent, pricelist, 5);
+            manager.GenerateBill(vehicle, TypeOfService.Sell, pricelist, 1);
+            Assert.AreEqual(122500,manager.TotalRevenue);
         }
     }
 }
